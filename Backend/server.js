472e0b027c83794ca69229fc0fc0761677b48dd0
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-12-21 12:01:49 +0530

    Added all the controllers

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./database/db.js");
+const bookRoutes = require("./routes/book-routes.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,8 +10,10 @@ const PORT = process.env.PORT || 3000;
 connectToDB();
 
 // Middleware -> express.json();
-
 app.use(express.json());
+
+// Routes
+app.use("/api/books", bookRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is now running.");
