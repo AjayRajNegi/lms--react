b2938d286fa781d5b7fea2c40df8b6084636dda5
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-12-23 13:41:38 +0530

    Get all files.

--- Backend/controllers/image-controller.js
+++ Backend/controllers/image-controller.js
@@ -36,6 +36,25 @@ const uploadImage = async (req, res) => {
   }
 };
 
+const fetchImages = async (req, res) => {
+  try {
+    const images = await Image.find({});
+    if (images) {
+      return res.status(200).json({
+        success: true,
+        data: images,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong! Please try again.",
+    });
+  }
+};
+
 module.exports = {
   uploadImage,
+  fetchImages,
 };
--- Backend/routes/image-routes.js
+++ Backend/routes/image-routes.js
@@ -2,16 +2,9 @@ const express = require("express");
 const authMiddleware = require("../middleware/auth-middleware");
 const adminMiddleware = require("../middleware/admin-middleware");
 const uploadMiddleware = require("../middleware/upload-middleware");
-const { uploadImage } = require("../controllers/image-controller");
+const { uploadImage, fetchImages } = require("../controllers/image-controller");
 
 const router = express.Router();
-
-console.log(
-  typeof authMiddleware,
-  typeof adminMiddleware,
-  typeof uploadMiddleware,
-  typeof uploadImage
-);
 
 router.post(
   "/upload",
@@ -20,5 +13,7 @@ router.post(
   uploadMiddleware.single("image"),
   uploadImage
 );
+
+router.get("/get", authMiddleware, fetchImages);
 
 module.exports = router;
